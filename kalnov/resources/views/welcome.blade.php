9cdb21a833b80d02125ee1224ba4066bf8b50df1
PlatfozaDmitriyVasilev <Foreverdiman2012>
2020-09-13 18:03:29 +0400

    initial laravel

---
+++ kalnov/resources/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae ea libero perferendis possimus, quos voluptas
+        voluptates? Architecto dolorem enim ex incidunt laudantium magni molestiae provident quidem, ratione sint unde.
+    </p>
+@endsection
