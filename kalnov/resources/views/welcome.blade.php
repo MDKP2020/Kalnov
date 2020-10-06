be22e9171030f7bb2ef05a7a42cc88640ac5ec77
Danila Kalnov <danyakalnov@gmail.com>
2020-10-06 21:37:39 +0400

    Изменён контент на главной странице на 'Учёт студентов'

--- kalnov/resources/views/welcome.blade.php
+++ kalnov/resources/views/welcome.blade.php
@@ -1,8 +1,5 @@
 @extends('layout')
 
 @section('content')
-    <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A beatae ea libero perferendis possimus, quos voluptas
-        voluptates? Architecto dolorem enim ex incidunt laudantium magni molestiae provident quidem, ratione sint unde.
-    </p>
+    <h2>Учёт студентов</h2>
 @endsection
