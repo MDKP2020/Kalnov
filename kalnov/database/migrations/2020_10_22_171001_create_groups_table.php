a429887354e62cc608ea2658ebe9b3975c0970a2
Данила Кальнов <danyakalnov@gmail.com>
2020-10-23 00:04:55 +0400

    Миграция для таблицы групп

---
+++ kalnov/database/migrations/2020_10_22_171001_create_groups_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->integer('number');
+            $table->foreignId('year_range_id')->constrained('year_ranges');
+            $table->foreignId('major_id')->constrained('majors');
+            $table->foreignId('previous_group_id')->nullable()->constrained();
+            $table->integer('year');
+            $table->date('last_exam_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
