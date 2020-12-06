7828949d65f54dbd7dd0032e9df52addee56be81
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-06 22:20:32 +0400

    Компонент списка групп

---
+++ kalnov/resources/js/components/GroupList.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect, useState } from "react";
+import ReactDOM from 'react-dom';
+
+export const GroupList = (props) => {
+
+
+    return (
+        <>
+            <h1>Работа со студентами</h1>
+            <div>
+
+            </div>
+        </>
+    )
+}
+
+ReactDOM.render(<GroupList/>, document.getElementById('groups-root'));
