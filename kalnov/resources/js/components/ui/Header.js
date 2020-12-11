eb928dca08d31cbc567f4e1af6828714734901de
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-11 23:01:31 +0400

    Стили хидера

--- kalnov/resources/js/components/ui/Header.js
+++ kalnov/resources/js/components/ui/Header.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import DeanLogo from '../../../img/icons/dean.svg'
+import {makeStyles, useTheme} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    header: {
+        display: 'flex',
+        alignItems: 'flex-end',
+        backgroundColor: theme.palette.background,
+        padding: "22px 60px"
+    },
+    logo: {
+        marginRight: '20px'
+    },
+    name: {
+        fontSize: '1.3rem'
+    }
+}))
 
 export const Header = (props) => {
+    const styles = useStyles(useTheme())
 
     return (
-        <div>
-            <img src={DeanLogo} alt="Деканат"/>
-            <span>Электронный деканат</span>
+        <div className={styles.header}>
+            <img src={DeanLogo} alt="Деканат" className={styles.logo}/>
+            <span className={styles.name}>Электронный деканат</span>
         </div>
     );
 }
