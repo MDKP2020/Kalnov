e09166b6dcf238fa91515d9ba419c3f66a547966
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-11 22:57:46 +0400

    Компонент выбора группы

--- kalnov/resources/js/components/group/GroupList.jsx
+++ kalnov/resources/js/components/group/GroupList.jsx
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from 'react-dom';
+import axios from '../../axios'
+import {makeStyles} from "@material-ui/core";
+import {Route, Switch} from "react-router";
+import {YearPicker} from "./YearPicker";
+
+const useStyles = makeStyles((theme) => ({
+    container: {
+        padding: ""
+    }
+}))
 
 export const GroupList = (props) => {
 
-
     return (
-        <>
+        <div>
             <h1>Работа со студентами</h1>
-            <div>
-
-            </div>
-        </>
+            <Switch>
+                <Route path='/groups'>
+                    <YearPicker/>
+                </Route>
+            </Switch>
+        </div>
     )
 }
