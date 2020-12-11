f96a41c1e56a7d47e919e027ccb5f88f407ed55b
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-11 19:11:36 +0400

    Тема фронтенда

---
+++ kalnov/resources/js/theme.js
@@ -0,0 +1,13 @@
+import {createMuiTheme} from "@material-ui/core";
+
+export const theme = createMuiTheme({
+    palette: {
+        primary: '#6099B9',
+        background: '#C4C4C4',
+        error: '#D31F1F',
+        text: {
+            main: '#202020',
+            light: '#888888'
+        }
+    }
+})
