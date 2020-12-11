8f651f07aa83d4b5dad2264fbb9e6b6658bd9709
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-11 22:58:31 +0400

    Исправлены цвета темы

--- kalnov/resources/js/theme.js
+++ kalnov/resources/js/theme.js
@@ -2,9 +2,13 @@ import {createMuiTheme} from "@material-ui/core";
 
 export const theme = createMuiTheme({
     palette: {
-        primary: '#6099B9',
+        primary: {
+            main: '#6099B9'
+        },
         background: '#C4C4C4',
-        error: '#D31F1F',
+        error: {
+            main: '#D31F1F'
+        },
         text: {
             main: '#202020',
             light: '#888888'
