65abb626940c415958931d4df7fa3f59465d62bb
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-11 17:41:27 +0400

    Статический метод создания и сохранения в БД года обучения

--- kalnov/app/Models/YearRange.php
+++ kalnov/app/Models/YearRange.php
@@ -4,9 +4,18 @@
 namespace App\Models;
 
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class YearRange extends Model
 {
+    protected $table = 'year_ranges';
 
+    public static function store(Request $request) {
+        $yearRange = new YearRange;
+        $yearRange->start = $request->input('start');
+
+        $yearRange->saveOrFail();
+    }
 }
