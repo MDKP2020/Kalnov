894edad2bbc75a6a3964654417589ffdb5f0fe99
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-12 15:44:44 +0400

    Стилизация компонента выбора года обучения

--- kalnov/resources/js/components/group/YearPicker.js
+++ kalnov/resources/js/components/group/YearPicker.js
@@ -1,10 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../../axios'
 import {useHistory} from "react-router";
+import {makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+    container: {
+        padding: '20px 0px'
+    },
+    yearContainer: {
+        paddingLeft: "15px"
+    },
+    year: {
+        display: 'block',
+        padding: '0.5rem 0',
+        cursor: 'pointer'
+    },
+    chooseYearLabel: {
+        fontSize: '1.5rem',
+        marginBottom: '1rem',
+        display: 'block'
+    }
+})
 
 export const YearPicker = (props) => {
 
     const [ years, setYears ] = useState([])
+    const styles = useStyles()
 
     useEffect(() => {
         axios.get('/years').then(yearsList => {
@@ -14,20 +35,28 @@ export const YearPicker = (props) => {
 
     const history = useHistory()
 
-    const handleYearClick = () => {
-        history.push(`/groups/${year.start}`)
+    const handleYearClick = (year) => {
+        history.push(`/groups/${year}`)
     }
 
     return (
-        <div>
-            <span>Выберите год обучения: </span>
-            { years.map(year => {
-                const date = new Date(year.start)
-                const currentYear = date.getFullYear()
-                const nextYear = date.getFullYear() + 1
+        <div className={styles.container}>
+            <span className={styles.chooseYearLabel}>Выберите год обучения: </span>
+            <div className={styles.yearContainer}>
+                { years.map(year => {
+                    const date = new Date(year.start)
+                    const currentYear = date.getFullYear()
+                    const nextYear = date.getFullYear() + 1
 
-                return <span onClick={handleYearClick}>{ `${currentYear} - ${nextYear}` }</span>
-            })}
+                    return <span
+                        key={currentYear}
+                        onClick={() => handleYearClick(currentYear)}
+                        className={styles.year}
+                    >
+                    { `${currentYear} - ${nextYear}` }
+                </span>
+                })}
+            </div>
         </div>
     )
 }
