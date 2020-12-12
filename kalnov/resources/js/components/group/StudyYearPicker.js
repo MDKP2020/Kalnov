9a29356d9ef3fa587829035fa483dcd6aaad7823
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-12 20:44:43 +0400

    GradePicker переименован в StudyYearPicker

--- kalnov/resources/js/components/group/StudyYearPicker.js
+++ kalnov/resources/js/components/group/StudyYearPicker.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from '../../axios'
 
-export const GradePicker = ({ year }) => {
+export const StudyYearPicker = ({ year }) => {
 
     return (<div></div>)
 }
