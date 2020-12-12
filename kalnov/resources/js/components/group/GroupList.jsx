872d9f986ddf0e31f8657ac09394bcb751b08ec4
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-12 15:45:50 +0400

    Добавлены отступы в компоненте выбора группы

--- kalnov/resources/js/components/group/GroupList.jsx
+++ kalnov/resources/js/components/group/GroupList.jsx
@@ -7,14 +7,16 @@ import {YearPicker} from "./YearPicker";
 
 const useStyles = makeStyles((theme) => ({
     container: {
-        padding: ""
+        padding: "20px 60px 0"
     }
 }))
 
 export const GroupList = (props) => {
 
+    const styles = useStyles()
+
     return (
-        <div>
+        <div className={styles.container}>
             <h1>Работа со студентами</h1>
             <Switch>
                 <Route path='/groups'>
