6928bbb5d205cd5e10c2c9d7cbed066ac2efd07d
nikitakalnov <jdmcorolla21@gmail.com>
2021-01-06 14:15:57 +0300

    Стилизация стандартной кнопки

--- kalnov/resources/js/components/ui/DeanButton.js
+++ kalnov/resources/js/components/ui/DeanButton.js
@@ -1,9 +1,43 @@
 import React from 'react'
-import { Button } from '@material-ui/core'
+import {Button, makeStyles, useTheme} from '@material-ui/core'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: '4px 16px', // change default padding for the button
+        fontWeight: 500, // default font-weight
+        letterSpacing: '0.04em', // default letter-spacing
+    },
+    primary: {
+        backgroundColor: theme.palette.primary.main,
+        color: 'white',
+        '&:hover': {
+            backgroundColor: theme.palette.primary.dark
+        }
+    },
+    secondary: {
+        backgroundColor: theme.palette.background.main,
+        color: 'black',
+        '&:hover': {
+            backgroundColor: theme.palette.background.dark
+        }
+    }
+}))
 
 export const DeanButton = ({ primary, secondary, error, children }) => {
+
+    const styles = useStyles(useTheme())
+    const classes = { root: styles.root }
+
+    const buttonClasses = []
+    if(primary)
+        buttonClasses.push(styles.primary)
+    else if(secondary)
+        buttonClasses.push(styles.secondary)
+
     return (<Button
         variant='contained'
         disableElevation
+        classes={classes}
+        className={buttonClasses.join(' ')}
     >{ children }</Button>)
 }
