a1d1aafd8b6c9c2a38336300e47f8ad94275a50b
nikitakalnov <jdmcorolla21@gmail.com>
2021-01-06 13:32:18 +0300

    Хук для переиспользования стандартных стилей

---
+++ kalnov/resources/js/hooks/useDefaultStyles.js
@@ -0,0 +1,9 @@
+import {makeStyles} from "@material-ui/core";
+
+export const useDefaultStyles = makeStyles(theme => ({
+    chooseLabel: {
+        fontSize: '1.4rem',
+        marginBottom: '1rem',
+        display: 'block'
+    }
+}))
