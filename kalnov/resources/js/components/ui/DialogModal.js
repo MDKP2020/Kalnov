b78977610068f318516c9d26b6fe8dc1af144323
2Kalnov <jdmcorolla21@gmail.com>
2021-01-18 22:58:34 +0300

    Пробрасывание пропов диалогового окна

--- kalnov/resources/js/components/ui/DialogModal.js
+++ kalnov/resources/js/components/ui/DialogModal.js
@@ -2,9 +2,9 @@ import React from 'react'
 import {Dialog, DialogActions, DialogTitle, DialogContent, DialogContentText} from "@material-ui/core";
 import {DeanButton} from "./DeanButton";
 
-export const DialogModal = ({ id, open, title, text, children, onClose, buttonText }) => {
+export const DialogModal = ({ id, open, title, text, children, onClose, buttonText, ...dialogProps }) => {
     return (
-        <Dialog aria-labelledby={id} open={open} onClose={onClose}>
+        <Dialog aria-labelledby={id} open={open} onClose={onClose} {...dialogProps}>
             <DialogTitle id={id}>{ title }</DialogTitle>
             <DialogContent>
                 <DialogContentText>{ text }</DialogContentText>
