a133e4d6c645d4f15d9682d84bec15fb79f9377d
Данила Кальнов <danyakalnov@gmail.com>
2021-01-21 17:51:57 +0300

    Добавлен метод для перевода студента из одной группы в другую

--- kalnov/app/Models/Student.php
+++ kalnov/app/Models/Student.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\MessageBag;
 
 class Student extends Model
 {
@@ -43,5 +44,34 @@ class Student extends Model
 
         if($wasEdited)
             $this->save();
+    }
+
+    public function transferToGroup($previousGroupId, $newGroupId) {
+        $validator = Validator::make(['previousGroupId' => $previousGroupId, 'newGroupId' => $newGroupId], [
+            'previousGroupId' => ['required', 'integer'],
+            'newGroupId' => ['required', 'integer'],
+        ]);
+
+        $previousGroup = Group::where('id', '=', $previousGroupId);
+        if ($previousGroupId->exists())
+            throw new BadRequestException(new MessageBag([error => "Group with id $previousGroupId does not exist"]));
+
+        $newGroup = Group::where('id', '=', $newGroupId);
+        if ($newGroup->exists())
+            throw new BadRequestException(new MessageBag([error => "Group with id $newGroupId does not exist"]));
+
+        $canTransferToNewGroup =
+            $newGroup->get()->getAttribute('study_year') == $previousGroup->get()->getAttribute('study_year') &&
+            $newGroup->get()->getAttribute('year_range') == $previousGroup->get()->getAttribute('year_range');
+
+        if ($validator->fails())
+            throw new BadRequestException($validator->errors());
+
+        if ($canTransferToNewGroup)
+            StudentRecord::where('student_id', '=', $this->getAttribute('student_id'))
+                ->where('group_id', '=', $previousGroupId)
+                ->update(['group_id' => $newGroupId]);
+        else
+            throw new BadRequestException(new MessageBag([error => 'Can not transfer student to such group: not the same year range or study year']));
     }
 }
